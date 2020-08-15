c2a5f5b56bdd2e226314cd21660b22152eff6e88
685195 <685195@host.docker.internal>
2020-08-16 01:27:59 +0530

    Adding a sample commit

--- src/main/java/com/prazy/mc/bankofprazy/controllers/UserRegistration.java
+++ src/main/java/com/prazy/mc/bankofprazy/controllers/UserRegistration.java
@@ -9,7 +9,7 @@ public class UserRegistration {
 	@PostMapping("/register")
 	public String registerUser() {
 		
-		return "User Registered successfully in Bank of Prazy (BoP). Thanks!";
+		return "User Registered successfully in Bank of Prazy (BoP)...Thanks!";
 	}
 
 }
