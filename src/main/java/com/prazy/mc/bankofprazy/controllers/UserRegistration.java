a7ccf15f9605018ba0a2d9945daea2b501f51eda
685195 <685195@host.docker.internal>
2020-08-16 03:51:09 +0530

    Changing to JSON response

--- src/main/java/com/prazy/mc/bankofprazy/controllers/UserRegistration.java
+++ src/main/java/com/prazy/mc/bankofprazy/controllers/UserRegistration.java
@@ -1,15 +1,21 @@
 package com.prazy.mc.bankofprazy.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserRegistration {
 	
-	@PostMapping("/register")
-	public String registerUser() {
+	@PostMapping(path="/register", produces="application/json; charset=UTF-8")
+	public ResponseEntity<String> registerUser() {
 		
-		return "User Registered successfully in Bank of Prazy (BoP), Welcome!";
+		String msg = "User Registered successfully in Bank of Prazy (BoP), Welcome!";
+		
+		String jsonResponse = "{ \"msg\":\""+msg+"\" }";
+		
+		return new ResponseEntity<String>(jsonResponse,HttpStatus.OK);
 	}
 
 }
