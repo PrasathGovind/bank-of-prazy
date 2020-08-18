b77598f7a49bfa22931c07604b510270661bb7e1
685195 <685195@LTIN140932.cts.com>
2020-08-19 00:54:14 +0530

    Removing aKey and sKey

--- src/main/java/com/prazy/mc/bankofprazy/configs/DynamoDBConfig.java
+++ src/main/java/com/prazy/mc/bankofprazy/configs/DynamoDBConfig.java
@@ -16,9 +16,9 @@ import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 @Configuration
 public class DynamoDBConfig {
 	
-	private String aKey = "AKIAZH6W6RYTT4NEOEYN";
+	private String aKey = "";
 	
-	private String sKey = "Eyl6z26nNCoRbZ6m1FEaD7En8d2tnYzdslw76n98";
+	private String sKey = "";
 	
 	@Value("${amazon.region}")
 	private String awsRegion;
